47103be771af43fa4520f9ab69b58e8fa65057b0
Muslim <khamkhoev.muslim@gmail.com>
2023-11-19 19:57:29 +0300

    second commit

--- page_login.php
+++ page_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "register.php"
+require "functions.php"
 ?>
 
 <!DOCTYPE html>
--- register.php
+++ register.php
@@ -2,42 +2,8 @@
 session_start();
 require "functions.php";
 
-$email = "Muslim1@example.com";
-$password = "secret";
-
-$pdo = new PDO("mysql:host=localhost;dbname=my_project", "root", "" );
-
-$sql = "SELECT * FROM users WHERE email=:email";
-$statement = $pdo->prepare($sql);
-$statement->execute(["email" => $email]);
-$user = $statement->fetch(PDO::FETCH_ASSOC);
-
-if(!empty($user)){
-    $_SESSION["danger"] = "Этот элекстронный адрес уже занят lheubv gjkmpjdfntktv";
-    header("Location:/page_register.php");
-    exit;
-}
-
-
-
-$sql = "INSERT INTO users(email, password) VALUES (:email, :password)";
-$statement = $pdo->prepare($sql);
-$statement->execute(
-    [
-        "email" => $email,
-        "password" => password_hash($password, PASSWORD_DEFAULT)
-    ]);
-
-$_SESSION["success"] = "Регистрация прошла успешно";
-header("Location:/page_login.php");
-exit;
-
-
-
-/*
-
-$email = "Muslim@example.com";        //$_POST["email"];
-$password = "123";                     //$_POST["password"];
+$email = $_POST["email"];
+$password =$_POST["password"];
 
 $user = get_user_by_email($email);
 
@@ -51,4 +17,3 @@ add_user($email, $password);
 set_flash_message("success", "Рeгистрация успешна");
 
 redirect_to("/page_login.php");
-*/
